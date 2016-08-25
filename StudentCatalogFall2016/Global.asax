378e53510284e904e8bca7709053bd4863d7154d
Christian <Christian@WINDOWS-QHV7H21>
2016-08-25 10:35:08 +0200

    Add project files.

---
+++ StudentCatalogFall2016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentCatalogFall2016.MvcApplication" Language="C#" %>
---
+++ StudentCatalogFall2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentCatalogFall2016.Startup))]
+namespace StudentCatalogFall2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
